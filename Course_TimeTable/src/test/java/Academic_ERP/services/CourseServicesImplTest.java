d84dcefb0a65a36e2d656094b4c1a17c88bd8b69
Manthan Fursule <manthanfursule19@gmail.com>
2020-11-23 11:35:49 +0530

    Added compareArrayList functionality

--- Course_TimeTable/src/test/java/Academic_ERP/services/CourseServicesImplTest.java
+++ Course_TimeTable/src/test/java/Academic_ERP/services/CourseServicesImplTest.java
@@ -1,6 +1,8 @@
 package Academic_ERP.services;
 
 import Academic_ERP.bean.Course;
+import Academic_ERP.bean.Student;
+import Academic_ERP.bean.Student_Min;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -11,35 +13,49 @@ import static org.junit.jupiter.api.Assertions.*;
 class   CourseServicesImplTest {
 
     CourseServicesImpl csi = new CourseServicesImpl();
+
     @Test
     void findStudentsEnrolled() {
+        List<Student> stu = csi.findStudentsEnrolled("CS503");
+        Student s1 = new Student("IMT2019CS2","Rohith Shetty");
+        Student s2 = new Student("IMT2019CS5","Vivek Bhat");
+        Student s3 = new Student("IMT2019CS8","Prathibha");
+        List<Student> st = new ArrayList<>();
+        st.add(s1);
+        st.add(s2);
+        st.add(s3);
+        assertTrue(arrayCompareStudents(stu,st));
     }
 
     @Test
-    void findCourseBySlotInDomain1() {
+    void findCourseBySlotInDomain() {
 
         Course test = new Course("CS101", "Algorithms", "Muralidhara V N");
         List<Course> c1 = new ArrayList<>();
         c1.add(test);
         List<Course> c2 = csi.findCourseBySlotInDomain(1, "MTech CSE");
-        //System.out.println(c1.get(0).equals(c2.get(0)));
-        assertEquals(c1.size(), c2.size());
+        assertTrue(arrayCompareCourses(c1,c2));
     }
-    @Test
-    void findCourseBySlotInDomain2(){
-        List<Course> c2 = csi.findCourseBySlotInDomain(3, "MTech CSE");
-        assertEquals(0, c2.size());
-    }
-    @Test
-    void findCourseBySlotInDomain3(){
-        List<Course> c2 = csi.findCourseBySlotInDomain(5, "MTech CSE");
-        assertEquals(2, c2.size());
-    }
-
 
     @Test
     void findCourseBySlot() {
+        Course test1 = new Course("EC104", "Control Systems", "Sudarshan Patil Kulkarni");
+        Course test2 = new Course("CS505", "Maths", "Venkateshwara Bhat");
+        List<Course> c1 = new ArrayList<>();
+        c1.add(test1);
+        c1.add(test2);
         List<Course> cis = csi.findCourseBySlot(3);
+        //System.out.println(cis);
+        assertTrue(arrayCompareCourses(c1,cis));
 
+
+    }
+
+    boolean arrayCompareCourses(List<Course> cmp1, List<Course> cmp2) {
+        return cmp1.toString().contains(cmp2.toString());
+    }
+
+    boolean arrayCompareStudents(List<Student> std1, List<Student> std2){
+        return std1.toString().contains(std2.toString());
     }
 }
